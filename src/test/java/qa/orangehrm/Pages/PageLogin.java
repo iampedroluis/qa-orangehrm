8929f57b4e17711973d9eda2cc0aa94f7d707fd0
iampedroluis <pedroluisgutierrez96@gmail.com>
2025-03-08 22:49:51 -0300

    Add Gherkin with its configuration and develop the first test scenario in TDD

--- src/test/java/qa/orangehrm/Pages/PageLogin.java
+++ src/test/java/qa/orangehrm/Pages/PageLogin.java
@@ -1,7 +1,5 @@
 package qa.orangehrm.Pages;
 
-
-
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -10,29 +8,36 @@ import qa.orangehrm.ZevtTools.ZevTools;
 
 public class PageLogin {
 
-
-    private WebDriver driver;
     ZevTools zevtools = new ZevTools();
-    private PageDashBoard  dashBoard;
+    private WebDriver driver;
 
-    public PageLogin(WebDriver driver){
+    public PageLogin(WebDriver driver) {
         this.driver = driver;
-        this.dashBoard = new PageDashBoard(driver);
     }
 
-    public void loginUser ( String username, String password){
+    public void enterCredentials(String username, String password) {
+
         try {
             driver.findElement(By.xpath("//input[@name='username']")).sendKeys(username);
             driver.findElement(By.xpath("//input[@name='password']")).sendKeys(password);
             zevtools.sleepSeconds(2);
             zevtools.screenshot(driver);
-            driver.findElement(By.xpath("//button[normalize-space()='Login']")).click();
-            zevtools.sleepSeconds(2);
-            Assert.assertEquals(dashBoard.dashboardNavbarTitle(), "Dashboard");
         } catch (Exception e) {
             // TODO: handle exception
             zevtools.zevLogs("Error: " + e.getMessage(), "ERROR");
             e.printStackTrace();
         }
     }
+
+    public void clickLoginButton() {
+        try {
+            driver.findElement(By.xpath("//button[normalize-space()='Login']")).click();
+            zevtools.sleepSeconds(2);
+        } catch (Exception e) {
+            // TODO: handle exception
+            zevtools.zevLogs("Error: " + e.getMessage(), "ERROR");
+            e.printStackTrace();
+        }
+    }
+
 }
--- src/test/java/qa/orangehrm/ZevtTools/ZevTools.java
+++ src/test/java/qa/orangehrm/ZevtTools/ZevTools.java
@@ -90,4 +90,9 @@ public class ZevTools {
         return driver;
 
     }
+    
+    public void closeDriver(WebDriver driver) 
+    {
+        driver.quit();
+    }
 }
